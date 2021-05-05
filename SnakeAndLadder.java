528b0d83b83da11c39707e29a04b7629520c3ef8
ktritsh7 <ktriesh7@gmail.com>
2021-05-05 18:28:23 +0530

    Welcome to snake and ladder problem

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Snake And Ladder Problem");
+
+	}
+
+}
