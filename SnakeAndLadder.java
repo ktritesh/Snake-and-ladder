f8a67c64153febdf7a82411e876d8b84f6378b56
ktritsh7 <ktriesh7@gmail.com>
2021-05-07 22:42:50 +0530

    Added : Player roll a die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,6 +7,11 @@ public class SnakeAndLadder {
 		//Variable
 		int singlePlayer = 0;
 		System.out.println("Single player at start position is : " +singlePlayer);
+		
+		//computation
+        int roll = (int) (Math.random()*6) + 1;
+        
+        System.out.println("single player rolls the die which is : " + roll );
 
 	}
 
