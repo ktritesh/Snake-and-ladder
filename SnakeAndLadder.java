342db0d2eac5575481b99f8113566f29e0458938
ktritesh <82252410+ktritesh@users.noreply.github.com>
2021-05-07 22:48:29 +0530

    Update SnakeAndLadder.java

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,11 +4,11 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		
-		//Variable
-		int singlePlayer = 0;
-		System.out.println("Single player at start position is : " +singlePlayer);
+	//Variable
+	int singlePlayer = 0;
+	System.out.println("Single player at start position is : " +singlePlayer);
 		
-		//computation
+	//computation
         int roll = (int) (Math.random()*6) + 1;
         
         System.out.println("single player rolls the die which is : " + roll );
